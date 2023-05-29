5b75283e6df6929d09a3049caa506e25ba031a0d
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2023-05-29 15:37:03 +0300

    added retirementAge for constructor Man, Woman

--- src/main/java/Persons/Man.java
+++ src/main/java/Persons/Man.java
@@ -2,7 +2,7 @@ package Persons;
 
 public class Man extends Person {
     public Man(){
-
+        super.retirementAge = 65;
     }
     public Man(String firstName, String lastName, int age, String partner) {
         super(firstName, lastName, age, partner);
